0e104e3fc87b1e92cb19ee9f4fa244782c294535
albertexye <albertexye@gmail.com>
2024-04-14 23:15:24 -0400

    新增英语支持，并在第一次进入网站时弹出提示

--- index.js
+++ index.js
@@ -1,41 +1,67 @@
 import anime from "./third-party/anime.es.js";
 import gouzheList from "./data/gouzhe.json" with { type: "json" };
 
+const h1El = document.querySelector("h1.ranking");
 const bodyEl = document.querySelector("body");
 const mainEl = document.querySelector('main');
 const ulEl = document.querySelector('ul.ranking');
 
-function setTheme(isDark) {
-    if (isDark) {
-        bodyEl.classList.add('dark');
-        mainEl.classList.add('dark');
-        ulEl.classList.add('dark');
-    } else {
-        bodyEl.classList.remove('dark');
-        mainEl.classList.remove('dark');
-        ulEl.classList.remove('dark');
+let lang;
+let theme;
+
+// Color theme
+{
+    function setTheme() {
+        theme = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+        if (theme) {
+            bodyEl.classList.add('dark');
+            mainEl.classList.add('dark');
+            ulEl.classList.add('dark');
+        } else {
+            bodyEl.classList.remove('dark');
+            mainEl.classList.remove('dark');
+            ulEl.classList.remove('dark');
+        }
     }
+
+    setTheme();
+    window.matchMedia('(prefers-color-scheme: dark)')
+        .addEventListener('change', _=> { setTheme(); });
 }
 
-const isDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
-setTheme(isDark);
-window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
-    const isDark = event.matches;
-    setTheme(isDark);
-});
+// Language
+{
+    function setLang() {
+        const url = new URL(document.URL);
+        lang = Boolean((url.searchParams.get("lang") || navigator.language).match("^en.*$"));
+        if (lang) {
+            document.title = "Gouzhe Ranking";
+            h1El.innerText = "Gouzhe Ranking";
+        } else {
+            document.title = "狗者排行榜";
+            h1El.innerText = "狗者排行榜";
+        }
+    }
 
-for (const [i, gouzhe] of gouzheList.entries()) {
-    const innerHTML = `
+    setLang();
+    window.addEventListener("languagechange", setLang);
+}
+
+// JSON parsing
+{
+    for (const [i, gouzhe] of gouzheList.entries()) {
+        const innerHTML = `
 <a class="ranking" href="gouzhe/${gouzhe.name}/${gouzhe.name}.html">
-    <span class="ranking-number">${i+1}</span>
+    <span class="ranking-number">${i + 1}</span>
     <span class="ranking-name">${gouzhe.name}</span>
     <img class="ranking" src="data/photos/${gouzhe.photo}" alt="Gouzhe Image" />
 </a>
 `;
-    const liEl = document.createElement("li");
-    liEl.classList.add('ranking', 'hover');
-    liEl.innerHTML = innerHTML;
-    ulEl.appendChild(liEl);
+        const liEl = document.createElement("li");
+        liEl.classList.add('ranking', 'hover');
+        liEl.innerHTML = innerHTML;
+        ulEl.appendChild(liEl);
+    }
 }
 
 mainEl.style.display = 'block';
@@ -52,3 +78,12 @@ anime({
         easing: 'linear'
     }
 });
+
+// Disclaimer
+{
+    const disclaimerShown = localStorage.getItem("disclaimerShown") === "true";
+    if (!disclaimerShown) {
+        alert(lang ? "This site is just for fun. Don't take it serious. " : "这个网站只是一个玩笑，请勿当真。");
+        localStorage.setItem("disclaimerShown", "true");
+    }
+}
